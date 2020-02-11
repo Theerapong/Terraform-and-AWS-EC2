09e5aeb823ed6df29799daa891fd221b62690e3e
jam <kulawong@live.com>
2020-02-11 20:51:33 +0000

    commit

--- resources.tf
+++ resources.tf
@@ -2,7 +2,6 @@ resource "aws_vpc" "environment-example-two" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
-<<<<<<< HEAD
   tags = {
     Name = "terrform-aws-vpc-example-two"
   }
@@ -22,10 +21,3 @@ resource "aws_subnet" "subnet2" {
 
 
 
-=======
-  tags {
-    Name = "terraform-aws-vpc-example-two"
-  }
-}
-
->>>>>>> d5057923963ad03c9fbcadcf27415ef3674e6a3f
